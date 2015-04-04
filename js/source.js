ceba02d7f3bb38d66c5a26ee9831e6b35383dc14
kartamyshev <kostya85>
2015-04-04 22:42:35 +0300

    ref| es6

--- js/source.js
+++ js/source.js
@@ -1,96 +1,103 @@
-DOMTokenList.prototype.forEach = Array.prototype.forEach;
-var preloader = document.querySelector('.preloader');
-
-
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-
-var loaders = [
-    {
-        name: 'horizontal-chunks',
-        html: `
+var loaders = [{
+    name: 'horizontal-chunks',
+    html: `
             <span>&nbsp;</span>
             <span>&nbsp;</span>
             <span>&nbsp;</span>
             <span>&nbsp;</span>
             <span>&nbsp;</span>
         `
-    }, {
-        name: 'successively-circles',
-        html: `
+}, {
+    name: 'successively-circles',
+    html: `
             <div></div>
             <div></div>
             <div></div>
             <div></div>
             <div></div>
         `
-    }, {
-        name: 'adventurous-circles',
-        html: `
+}, {
+    name: 'adventurous-circles',
+    html: `
             <div class="sand"></div>
             <div class="goldenrod"></div>
             <div class="orange"></div>
         `,
-        extra: {
-            selectors: ['body', '.get-loader'],
-            _class: 'b-adventurous-circles'
-        }
-    }, {
-        name: 'running-line',
-        html: `
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-        `
-    }, {
-        name: 'filled-circle',
-        html: `
-            <div></div>
-        `
+    extra: {
+        selectors: ['body', '.get-loader'],
+        _class: 'b-adventurous-circles'
     }
-
-
+}, {
+    name: 'running-line',
+    html: `
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        `
+}, {
+    name: 'filled-circle',
+    html: `
+            <div></div>
+        `
+}
 ];
 
-!function setDefaultLoader() {
-    var rand = getRandomInt(0, loaders.length - 1);
-    //var rand = loaders.length - 1;
+var preloader = document.querySelector('.preloader');
 
-    preloader.classList.add(loaders[rand].name);
-    preloader.innerHTML = loaders[rand].html;
 
-    if ('extra' in loaders[rand]) {
-        loaders[rand].extra.selectors.forEach((selector) => {
-            document.querySelector(selector).classList.add(loaders[rand].extra._class)
-        });
+class Loader {
+    constructor() {
+        DOMTokenList.prototype.forEach = Array.prototype.forEach;
+        this.getLoader = this.chooseLoader.bind(this);
+        this.setDefaultLoader();
     }
 
-}();
 
+    static getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
 
-function chooseLoader() {
-    var rand = getRandomInt(0, loaders.length - 1);
-    var classToAdd = loaders[rand].name;
+    static setLoader(which) {
+        preloader.classList.add(loaders[which].name);
+        preloader.innerHTML = loaders[which].html;
+    }
 
-    preloader.classList.forEach(function(_class) {
-        if (_class !== 'preloader') {
-            preloader.classList.remove(_class);
+    setDefaultLoader() {
+        var _static = this.constructor,
+            rand = _static.getRandomInt(0, loaders.length - 1);
+
+        _static.setLoader(rand);
+
+        if ('extra' in loaders[rand]) {
+            loaders[rand].extra.selectors.forEach((selector) => {
+                document.querySelector(selector).classList.add(loaders[rand].extra._class)
+            });
         }
-    });
-
-    preloader.classList.add(classToAdd);
-    preloader.innerHTML = loaders[rand].html;
-
-    document.body.setAttribute('class', '');
-
-    if ('extra' in loaders[rand]) {
-        loaders[rand].extra.selectors.forEach((selector) => {
-            document.querySelector(selector).classList.add(loaders[rand].extra._class)
-        });
     }
+
+    chooseLoader() {
+        var _static = this.constructor,
+            rand = _static.getRandomInt(0, loaders.length - 1);
+
+        preloader.classList.forEach(function (_class) {
+            if (_class !== 'preloader') {
+                preloader.classList.remove(_class);
+            }
+        });
+
+        _static.setLoader(rand);
+
+        document.body.setAttribute('class', '');
+
+        if ('extra' in loaders[rand]) {
+            loaders[rand].extra.selectors.forEach((selector) => {
+                document.querySelector(selector).classList.add(loaders[rand].extra._class)
+            });
+        }
+    }
+
 }
 
-document.querySelector('.get-loader').addEventListener('click', chooseLoader, false);
+var loader = new Loader();
+document.querySelector('.get-loader').addEventListener('click', loader.getLoader, false);
